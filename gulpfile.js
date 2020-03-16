d0cbfd396a9f221731b259ed253d2c2126d18bca
Paul <paulvantuyl@users.noreply.github.com>
2020-03-15 20:54:48 -0700

    Rewrite syntax for gulp 4
    Not sire if this will work 🤞🏼

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,39 @@
-var gulp = require('gulp'), 
+const { series } = require('gulp'),
 	dependencies = require('gulp-web-dependencies');
-var path_dest = 'js/vendor'; // The basedir of your app
-var $ = require('gulp-load-plugins')();
-var sassPaths = [
+const $ = require('gulp-load-plugins')();
+const path_dest = 'js/vendor';
+const sassPaths = [
   'node_modules/foundation-sites/scss',
   'node_modules/motion-ui/src'
 ];
 
-gulp.task('sass', function() {
+function sass(cb) {
 	return gulp.src('scss/app.scss')
     .pipe($.sass({
     		includePaths: sassPaths,
-    		outputStyle: 'compressed' // if css compressed **file size**
-    })
-    	.on('error', $.sass.logError))
+    		outputStyle: 'compressed'
+    	})
+    	.on('error', $.sass.logError)
+    )
     .pipe($.autoprefixer({
     		browsers: ['last 2 versions', 'ie >= 9']
     }))
     .pipe(gulp.dest('css'));
-});
+	cb();
+}
 
-gulp.task('dependencies', function() {
-	return gulp.src([
+function dependencies(cb) {
+  return gulp.src([
     'node_modules/foundation-sites/dist/js/foundation.min.js',
     'node_modules/jquery/dist/jquery.min.js',
     'node_modules/what-input/dist/what-input.min.js'
 	])
 		.pipe(dependencies({
 			dest: path_dest,
-			prefix: '/vendor',  // The URL prefix. Default "/"
+			prefix: '/vendor',
 		}))
 		.pipe(gulp.dest(path_dest));
-});
+  cb();
+}
 
-// gulp.task('default', ['sass'], function() {
-//	gulp.watch(['scss/**/*.scss'], ['sass']);
-//	gulp.watch(['js/**/*.js'], ['js']);
-// })
-
-// Gulp Default Task
-gulp.task('default', ['dependencies', 'sass']);
+exports.build = series(sass, dependencies);
