f8156d21c6dc2196b67331ef488b75e73467056e
Paul <paulvantuyl@users.noreply.github.com>
2025-12-12 18:33:27 -0700

    Navigation updates

--- src/App.tsx
+++ src/App.tsx
@@ -8,34 +8,49 @@ function App() {
 
   return (
     <ThemeProvider attribute="class" defaultTheme="dark">
-      <div className="bg-midnight w-screen h-screen">
-      <NavigationMenu.Root className="relative z-10 flex w-screen justify-left bg-black">
-        <NavigationMenu.List className="center m-0 flex list-none rounded-none p-1">
-          <NavigationMenu.Item>
-            <NavigationMenu.Link
-              className="block select-none rounded-none px-4 py-2 text-sm leading-none no-underline outline-none focus:shadow-[0_0_0_2px]"
-              href="https://github.com/paulvantuyl"
-            >
-              Github
-            </NavigationMenu.Link>
-          </NavigationMenu.Item>
-          
-        </NavigationMenu.List>
-      </NavigationMenu.Root>
+      <div className="bg-midnight w-screen h-screen" id="monitor">
+        <NavigationMenu.Root className="relative z-10 flex w-screen justify-left bg-black">
+          <NavigationMenu.List className="center mb-0 mt-1 flex list-none rounded-none p-0">
+            <NavigationMenu.Item>
+              <NavigationMenu.Link
+                className="block h-full content-center select-none rounded-none px-4 pt-1 pb-2 focus:shadow-[0_0_0_2px]"
+                href="/"
+              >
+                <img src="/assets/phvt-triangle-white.svg" alt="Paul Van Tuyl" className="h-4" />
+              </NavigationMenu.Link>
+            </NavigationMenu.Item>
+            <NavigationMenu.Item>
+              <NavigationMenu.Link
+                className="block select-none rounded-none ps-4 pe-12 py-2 text-base leading-none no-underline outline-none bg-red focus:shadow-[0_0_0_2px]"
+                href="/"
+              >
+                Start
+              </NavigationMenu.Link>
+            </NavigationMenu.Item>
+            <NavigationMenu.Item>
+              <NavigationMenu.Link
+                className="block select-none rounded-none ps-4 pe-12 py-2 text-base leading-none no-underline outline-none focus:shadow-[0_0_0_2px]"
+                href="https://github.com/paulvantuyl"
+              >
+                Github
+              </NavigationMenu.Link>
+            </NavigationMenu.Item>
+          </NavigationMenu.List>
+        </NavigationMenu.Root>
 
-      <div className="flex flex-row">
-        <div className="basis-3/3">
-          <h1>Hello, World</h1>
-          <p><strong>Can</strong> enterprise software <em>be</em> interesting?</p>
-          <p>
-              <button className="rounded-none" onClick={() => setCount((count) => count + 1)}>
-                count is {count}
-              </button>
-          </p>
-          <p><small>This is a small text</small></p>
-          
+        <div className="flex flex-row p-2" id="content">
+          <div className="basis-3/3">
+            <h1>Hello, World</h1>
+            <p><strong>Can</strong> enterprise software <em>be</em> interesting?</p>
+            <p>
+                <button className="rounded-none" onClick={() => setCount((count) => count + 1)}>
+                  count is {count}
+                </button>
+            </p>
+            <p><small>This is a small text</small></p>
+            
+          </div>
         </div>
-      </div>
       </div>
     </ThemeProvider>
   )
