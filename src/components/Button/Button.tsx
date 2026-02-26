8837c4d717136972c936713e3445157478e48158
Paul Van Tuyl <paul@pvt.design>
2026-02-26 07:43:43 -0700

    Start moving button css to component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,13 @@
 import { forwardRef } from 'react'
 import type { ButtonProps } from './Button.types'
+import './Button.css'
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ children, className = '', ...props }, ref) => {
     return (
       <button
         ref={ref}
-        className={`rounded-none px-4 py-2 bg-blue hover:bg-dark-blue font-bold ${className}`}
+        className={className ? `button ${className}` : 'button'}
         {...props}
       >
         {children}
