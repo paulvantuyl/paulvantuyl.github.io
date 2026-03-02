0a6090eb9959f65036423b2173587eeeedf925bc
Paul Van Tuyl <paul@pvt.design>
2026-03-01 20:55:16 -0700

    Update SB to run Tailwind Vite and globally load app.css

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import tailwindcss from '@tailwindcss/vite';
 
 const config: StorybookConfig = {
   "stories": [
@@ -12,6 +13,11 @@ const config: StorybookConfig = {
     "@storybook/addon-docs",
     "@storybook/addon-onboarding"
   ],
-  "framework": "@storybook/react-vite"
+  "framework": "@storybook/react-vite",
+  async viteFinal(config) {
+    config.plugins = config.plugins || [];
+    config.plugins.push(tailwindcss());
+    return config;
+  }
 };
 export default config;
---
+++ storybook-static/assets/Button.stories-DzZShb39.js
@@ -0,0 +1,6 @@
+import{j as c}from"./jsx-runtime-u17CrQMm.js";import{r as d}from"./iframe-CZxhYELG.js";import"./preload-helper-PPVm8Dsz.js";const r=d.forwardRef(({children:a,className:s="",...o},n)=>c.jsx("button",{ref:n,className:s?`button ${s}`:"button",...o,children:a}));r.displayName="Button";r.__docgenInfo={description:"",methods:[],displayName:"Button",props:{children:{required:!0,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:""},className:{defaultValue:{value:"''",computed:!1},required:!1}},composes:["ButtonHTMLAttributes"]};const p={title:"Components/Button",component:r,tags:["autodocs"],args:{children:"Click me",type:"button"}},e={},t={args:{disabled:!0,children:"Disabled"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    children: 'Disabled'
+  }
+}`,...t.parameters?.docs?.source}}};const l=["Primary","Disabled"];export{t as Disabled,e as Primary,l as __namedExportsOrder,p as default};
