2995ea6f28e75c57293fb8453cb1d19f8ec09455
Paul <paulvantuyl@users.noreply.github.com>
2026-03-15 07:54:35 -0700

    Rewrite and refactor home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { Layout } from '../components/Layout';
 import { Text } from '../components/Text';
+import { Icon } from '../components/Icon';
 
 const sidebarContent = (
     <Text variant="h5" className="post-tags">Research&amp;<br />Pixels&amp;<br />Protypes&amp;<br />Code</Text>
@@ -10,132 +11,44 @@ const subTitle = "Developer/designer working on B2B, B2C and enterprise products
 export function Home() {
     return (
         <Layout title="Hello, World" subtitle={subTitle} variant="sidebar" sidebarContent={sidebarContent}>
-            <p>Hi, I'm Paul Van Tuyl. In case you're wondering, my name is pronounced <em>Van Tile</em>. I'm a huge nerd who loves designing applications (mobile or SaaS). A few fun facts:</p>
-
-            <ul className="datalist">
-                <li>My wife and I have kids, and we try to help kids in the foster care system.</li>
-                <li>I'm more of a dog person, but let's be honest. Cats are really funny.</li>
-                <li>I work full-time as a Principal Product Designer at <a href="https://get.keap.com/do5da5ak3eui" title="Keap">Keap</a> in Chandler, AZ.</li>
-                <li>I'm a gamer.<i className="far fa-gamepad"></i></li>
-            </ul>
-
-            <h3>My personal mission statement</h3>
-
-            <p><em>My professional goal is to use my curiosity, love for design, and craftsmanship to create products, processes, and objects that improve quality of life for human beings.</em></p>
+            <Text variant="p">Hi, I'm Paul Van Tuyl (<span aria-label="Van Tile" lang="en-fonipa">/v&aelig;n ta&#618;l/</span>). Professionally I spend my time in a hybrid role with the primary focus on product design, and the secondary focus on development. I'm looking to switch that up.</Text>
 
             <h2>Résumé</h2>
 
-            <p>I've been working as a designer full time since 2006. While my education was in graphic design for print, the majority of my experience has been in digital formats. I fell in love with the web while in school, and come from the era (and a place) where you had to be able to both design and develop your work.</p>
+            <p>I've been working as a designer or developer (primarily frontend) full time since 2006. While my education was in graphic design, my professional focus has been centered around digital products. I started making things for the web while in school, and come from the era where you had to be able to both design and develop your work.</p>
 
-            <p>I think that design isn't just how things look. It's how it works, looks, and makes you feel.</p>
+            <Text variant="h3">Thryv</Text>
+            <Text variant="p">Principal Product Designer<br />
+                Dallas, TX (Remote)<br />
+                July 2018 &ndash; Present</Text>
+            <Text variant="p">As a Principal Product Designer at <a href="https://www.thryv.com/">Thryv</a>, I fill a hybrid role of designer and developer. My biggest focus is helping to architect a design system that's multi-framework, multi-brand, and multi-theme.</Text>
+            <Text variant="p">Originally, I started at Infusionsoft in 2018, which later rebranded as Keap. In late 2024, Keap was acquired by Thryv.</Text>  
 
-            <h3>Experience</h3>
+            <Text variant="h3">Tallwave</Text>
+            <Text variant="p">Design Team Lead<br />
+                Scottsdale, AZ<br />
+                January, 2015 &ndash; July, 2018</Text>
+            <Text variant="p">As a Design Team Lead at <a href="https://tallwave.com/">Tallwave</a>, I helped guide efforts to implement design thinking as we created business solutions and opportunities &ndash; through well-crafted experiences, products, and services &ndash; for Tallwave's clients. I worked directly with clients to help define strategy and requirements for projects and direct team members how to execute against specific business outcomes effectively and efficiently.</Text>
+            <Text variant="p">Within the Design Team at Tallwave, my role extended into several areas. Repeatable and effective design process was important at Tallwave, so I worked on ways that we could improve our workflows, methods, and production pipelines. Design Team Leads were also responsible for reviewing work and providing feedback, mentoring team members, scoping projects, and trying to unlock creative potential.<br />
+                <small>Other positions held: Emotional Design Lead, Sr. Product Designer</small></Text>
 
-            <p>Thryv</p>
+            <Text variant="h3">29th Drive</Text>
+            <Text variant="p">Sr. Product Designer<br />
+                Scottsdale, AZ<br />
+                September, 2013 &ndash; December, 2014</Text>
+            <Text variant="p">At 29th Drive, we primarily worked as a boutique UX Design firm. Team members had to be cross-disciplinary; my responsibilities included UX Design, UI Design, Front End Development, Brand Design, and Email Design.<br />
+                <small>Other positions held: Product Designer &amp; Front End Developer</small></Text>
 
-            <dl>
-                <dt>
-                    Staff Product Designer<br />
-                    Keap
-                </dt>
-                <dd>
-                    <p><span className="text-muted">Chandler, AZ (Remote)</span><br />
-                        <span className="text-muted">July 2018 &ndash; Present</span></p>
-                    <p>As a Staff Product Designer at <a href="https://get.keap.com/do5da5ak3eui">Keap</a>, I fill a hybrid role of people leader and individual contributor. You may find me:</p>
-                    <ul className="datalist">
-                        <li>Helping with design sprints</li>
-                        <li>Working with PMs, Researchers, Engineers, and other Designers to concept and develop end-to-end desktop and mobile-responsive experiences that deliver on customer and business outcomes</li>
-                        <li>Working with team members to validate design using customer research and leverage analytics to make data-informed decisions</li>
-                        <li>Helping the designers that work with me level up their careers</li>
-                        <li>Championing and improving our design system</li>
-                    </ul>
-                    <p><small>Other positions held: Principal Product Designer</small></p>
-                </dd>
-            </dl>
+            <Text variant="h3">Education &amp; Certifications</Text>
 
-            <dl>
-                <dt>
-                    Design Team Lead<br />
-                    Tallwave
-                </dt>
-                <dd>
-                    <p><span className="text-muted">Scottsdale, AZ</span><br />
-                        <span className="text-muted">January, 2015 &ndash; July, 2018</span></p>
-                    <p>As a Design Team Lead at <a href="https://tallwave.com/">Tallwave</a>, I helped guide efforts to implement design thinking as we created business solutions and opportunities &ndash; through well-crafted experiences, products, and services &ndash; for Tallwave's clients. I worked directly with clients to help define strategy and requirements for projects and direct team members how to execute against specific business outcomes effectively and efficiently.</p>
-                    <p>Within the Design Team at Tallwave, my role extended into several areas. Repeatable and effective design process is important at Tallwave, so I worked on ways that we could improve our workflows, methods, and production pipelines. Design Team Leads were also responsible for reviewing work and providing feedback, mentoring team members, scoping projects, and trying to unlock creative potential.<br />
-                        <small>Other positions held: Emotional Design Lead, Sr. Product Designer</small></p>
-                </dd>
-            </dl>
-
-            <dl>
-                <dt>
-                    Sr. Product Designer<br />
-                    29th Drive
-                </dt>
-                <dd>
-                    <p><span className="text-muted">Scottsdale, AZ</span><br />
-                        <span className="text-muted">September, 2013 &ndash; December, 2014</span></p>
-                    <p>At 29th Drive, we primarily worked as a boutique UX Design firm. Team members had to be cross-disciplinary; my responsibilities included UX Design, UI Design, Front End Development, Brand Design, and Email Design.<br />
-                        <small>Other positions held: Product Designer &amp; Front End Developer</small></p>
-                </dd>
-            </dl>
-
-            <h3>Amazing customers</h3>
-
-            <p>Some of the customers I've had the great opportunity to work with throughout my career include ASU, American Express, AppointmentPlus, CBANC, Cybersponse, Full Contour, Gibson Dunn, Isagenix, author John Dickerson, Minitab, PayPal, and Weave Education.</p>
-
-            <h3>Speaking engagements</h3>
-
-            <dl>
-                <dt>Phoenix Design Week, 2017</dt>
-                <dd>
-                    <span className="text-muted">Phoenix, AZ</span><br />
-                    Presenter for a User Experience Breakout Session, <em>Design consistency, craft, efficiency, and cat memes</em>
-                </dd>
-            </dl>
-            <span className="three-em-dash"></span>
-
-            <dl>
-                <dt>Phoenix Design Week, 2015</dt>
-                <dd>
-                    <span className="text-muted">Phoenix, AZ</span><br />
-                    Co-presenter for a Startups track workshop, <em>5 Product &amp; Business Design Secrets for Designers with Great Ideas</em>
-                </dd>
-            </dl>
-            <span className="three-em-dash"></span>
-
-            <dl>
-                <dt>UX Week, 2014</dt>
-                <dd>
-                    <span className="text-muted">San Francisco, CA</span><br />
-                    Co-presenter for a UX Week Workshop, <em>Design Studios &amp; Pen Sketching for Makers of Apps &amp; Websites</em>
-                </dd>
-            </dl>
-
-            <h3 className="mt-4">Education</h3>
-            <dl>
-                <dt>
-                    Remote Work Certification
-                </dt>
-                <dd>
-                    <span className="text-muted">Workplaceless</span><br />
-                    <span className="text-muted">March, 2021</span><br /><br />
-                    <a href="https://www.credential.net/57ec24d5-d706-473d-ad01-63206bc18caf" className="thumbnail">
-                        <img src="https://api.accredible.com/v1/frontend/credential_website_embed_image/badge/30724418" />
-                    </a>
-                </dd>
-            </dl>
-
-            <dl>
-                <dt>
-                    Associate of Applied Science in Graphic Design
-                </dt>
-                <dd>
-                    <span className="text-muted">Yavapai College</span><br />
-                    <span className="text-muted">May, 2006</span><br />
-                    Graduated with honors <i className="far fa-laugh-beam"></i>
-                </dd>
-            </dl>
+            <Text variant="p"><strong>IBM Full-Stack JavaScript Developer</strong><br />
+                <a href="https://coursera.org/share/7e14260df572e33398679f7f5ce42a77">IBM via Coursera</a><br />
+                February, 2026</Text>
+            
+            <Text variant="p"><strong>Associate of Applied Science in Graphic Design</strong><br />
+                Yavapai College<br />
+                May, 2006<br />
+                Graduated with honors <Icon name="laugh-beam" className="inline"></Icon></Text>
         </Layout>
     )
 }
