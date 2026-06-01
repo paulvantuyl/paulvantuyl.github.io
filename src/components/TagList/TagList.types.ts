923a618fab3b48811d2dc78ba08eb9ffb9374032
Paul Van Tuyl <paul@pvt.design>
2026-05-31 21:02:22 -0700

    fix: Update types and import text variants

--- src/components/TagList/TagList.types.ts
+++ src/components/TagList/TagList.types.ts
@@ -1,9 +1,11 @@
 import type { HTMLAttributes } from 'react'
+import type { TextVariant } from '../Text/Text.types'
 
 export interface TagListItem {
-  value: string
+  value: string,
 }
 
 export interface TagListProps extends HTMLAttributes<HTMLDivElement> {
   tags: TagListItem[]
+  variant?: TextVariant
 }
